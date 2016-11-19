fdd67cd896e825c7e9180c4373d6497019a742ac
Mike Bundy <mike.bundy@gmail.com>
2016-11-19 09:42:06 -0600

    Random Integer with and without range

---
+++ JunkDrawerDriver/JunkDrawer/JDRandom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunkDrawer
+{
+    public class JDRandom
+    {
+        private Random _random;
+
+        public JDRandom()
+        {
+            _random = new Random();
+        }
+
+        public int GetInteger()
+        {
+            return _random.Next();
+        }
+
+        public int GetInteger(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+    }
+}
---
+++ JunkDrawerDriver/JunkDrawerDriver/Program.cs
@@ -0,0 +1,20 @@
+﻿using JunkDrawer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunkDrawerDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JDRandom r = new JDRandom();
+            Console.WriteLine(r.GetInteger());
+            Console.WriteLine(r.GetInteger(32, 89));
+            Console.Read();
+        }
+    }
+}
